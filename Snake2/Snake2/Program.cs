05d9d7cb9a8cabea274a879c7eefda67cde9f2e8
akarnau <artur.karnauchow@gmail.com>
2016-10-22 23:48:11 +0600

    First commit

---
+++ Snake2/Snake2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.SetBufferSize(80, 25);
+
+            
+            HorizontalLine lineUp = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine lineDown = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine lineLeft = new VerticalLine(0, 24, 0, '+');
+            VerticalLine lineRight = new VerticalLine(0, 24, 78, '+');
+
+            lineLeft.DrawLine();
+            lineRight.DrawLine();
+            lineUp.DrawLine();
+            lineDown.DrawLine();
+
+            Point p = new Point(4, 5, '+');
+            p.Draw();
+
+
+            Console.ReadLine();
+
+            
+        }
+
+
+
+    }
+}
